a15d0d706ea56dde5ee3bfb858ebddce7e35e928
enggcs <vkoder2013@gmail.com>
2016-11-05 20:24:21 +0530

    Update threadfun.c

--- src/threadfun.c
+++ src/threadfun.c
@@ -149,3 +149,4 @@ What you need to do is , each thread should print one word and give the control 
 this way each thread will keep on printing one word , in case last thread come, it should invoke 
 first thread. Printing will repeat until all the words are printed in paragraph. 
 Finally all threads should exit gracefully. What kind of synchronization will use?
+*/
